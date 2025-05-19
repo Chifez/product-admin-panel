a391e7b3e7f8e37add12ce76d41fe38dee87f88b
emmanuel <chifez1@gmail.com>
2025-05-19 18:12:29 +0100

    added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,33 @@ import { ToastProvider } from '@/components/ui/toast';
 import { Toaster } from '@/components/ui/sonner';
 
 export const metadata: Metadata = {
-  title: 'Product Management Dashboard',
-  description: 'Manage All your Product',
+  title: 'Inventrack - Product Management Dashboard',
+  description:
+    'Manage and track your products and inventories, all in one place',
+  openGraph: {
+    title: 'Inventrack - Product Management Dashboard',
+    description:
+      'Manage and track your products and inventories, all in one place',
+    type: 'website',
+    locale: 'en-US',
+    url: 'https://inventrack-admin.netlify.app',
+    images: [
+      {
+        url: '/opengraph-image.png',
+        width: 1200,
+        height: 620,
+        alt: 'Inventrack - Product Management Dashboard',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Inventrack - Product Management Dashboard',
+    description:
+      'Manage and track your products and inventories, all in one place',
+    images: ['/twitter-image.png'],
+  },
+  metadataBase: new URL('https://inventrack-admin.netlify.app'),
 };
 
 export default function RootLayout({
